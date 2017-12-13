2e6ea2ff9286c2f790c50d29b8c8eba827b42250
AndyDLP <34372561+AndyDLP@users.noreply.github.com>
2017-12-13 13:23:36 +0000

    Barely anything #2

--- New-OSTicket.ps1
+++ New-OSTicket.ps1
@@ -24,6 +24,7 @@
         [string]$Department = "",
 
         # Custom field
+        # 123
         [Parameter(HelpMessage = "The computer that the ticket was generated from, can be spoofed")]
         [string]$ComputerName = "$env:COMPUTERNAME",
 
