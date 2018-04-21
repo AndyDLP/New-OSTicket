5b265aec0324a3bf810911f257abb0cd7b71cb5d
Andy de la Pole <adlp@sky.com>
2018-04-21 22:07:09 +0100

    Re did parameters and fixed attachments

--- New-OSTicket.ps1
+++ New-OSTicket.ps1
@@ -1,73 +1,166 @@
 ﻿function New-OSTicket {
-    [CmdletBinding()]
-    Param (
-        [Parameter(Mandatory = $True, HelpMessage = "The ticket title / subject")]
-        [string]$Title,
+<#
+	.SYNOPSIS
+		Creates a new ticket on the specified OSTicket instance
 
-        [Parameter(Mandatory = $True, HelpMessage = "The ticket message / body")]
-        [string]$Message,
+	.DESCRIPTION
+		Creates a new ticket on the specified OSTicket instance
 
-        [Parameter(HelpMessage = "User forename")]
-        [string]$Forename = "",
+	.PARAMETER Subject
+		The ticket subject
 
-        [Parameter(HelpMessage = "User surname")]
-        [string]$Surname = "",
+	.PARAMETER Message
+		The ticket message / body
 
-        [Parameter(HelpMessage = "Email address of the user")]
-        [string]$Email = "",
+	.PARAMETER Name
+		The users full name
 
-        [Parameter(HelpMessage = "The contact phone number of the user")]
-        [string]$PhoneNumber = "",
+	.PARAMETER Email
+		Email address of the user
 
-        # Custom field
-        [Parameter(HelpMessage = "The department to create the ticket in, must match exactly")]
-        [string]$Department = "",
+	.PARAMETER PhoneNumber
+		Phone number of the submitter. If the extension is included, use a capital 'X' followed by the extension
 
-        # Custom field
-        [Parameter(HelpMessage = "The computer that the ticket was generated from, can be spoofed")]
-        [string]$ComputerName = "$env:COMPUTERNAME",
+	.PARAMETER IpAddress
+		The sending IpAddress (can be spoofed?)
 
-        [Parameter(HelpMessage = "The sending IP (can be spoofed?)")]
-        [string]$ip = "",
+	.PARAMETER topicID
+		The help topic ID to be associated with the ticket
 
-        [Parameter(HelpMessage = "The ticket cateogry, must match exactly")]
-        [string]$Category = "",
+	.PARAMETER MessageType
+		The format type of the message, either text/html or text/plain
 
-        [Parameter(HelpMessage = "Topic ID number from ticket")]
-        [string]$topicID = "1",
+	.PARAMETER Alert
+		Send an alert to agents on ticket creation
 
-        # Custom field
-        [Parameter(HelpMessage = "Room")]
-        [string]$room = "",
+	.PARAMETER APIKey
+		The API key to send to the OSTicket install
 
-        [Parameter(HelpMessage = "The API key to send to the OSTicket install")]
-        [string]$APIKey = "",
+	.PARAMETER AutoRespond
+		Automatically respond with the ticket info to the email defined in ticket creation
 
-        [Parameter(HelpMessage = 'The application URI to send the POST request to e.g. [url]/api/tickets.json')]
-        [string]$URI = ""
-    )
+	.PARAMETER Server
+		The server to send the API call to. Can be FQDN or IP
 
-    $Params = @{
-        name = "$Forename $Surname";
-        email = $Email;
-        phone = $PhoneNumber;
-        Department = $Department;
-        subject = $Title;
-        ip = $ip;
-        message = $Message;
-        topicID = $topicID;
+	.PARAMETER Priority
+		The priority ID for the new ticket to assume
 
-        # Custom fields
-        FirstName = $Forename; # Custom
-        LastName = $Surname; # Custom
-        Category = $Category; # Custom
-        ComputerName = $ComputerName; # Custom
-        room = $room; # Custom
-        Title = $Title; # Custom
-        Body = $Message; # Custom
-        # Todo: fix this
-        #attachments = @{"Filename.txt" = ()}
-    } | ConvertTo-Json
+	.PARAMETER CustomFields
+		A list of custom (not present in base install) fields and their desired values
 
-    Invoke-WebRequest -Uri $URI -Headers @{'X-API-Key'=$APIKey} -Body $Params -UseDefaultCredentials -Method Post -ContentType "application/json"
+	.PARAMETER TicketSource
+		The ticket source type, such as email, phone or API
+
+	.PARAMETER Attachments
+		An array of file paths to attach to the initial message
+
+	.EXAMPLE
+		PS C:\> New-OSTicket -Subject 'Please Fix Microwave' -Message 'Someone put fish in it and no one wants to go near it' -Name 'Random User' -Email 'test@example.com'
+
+	.NOTES
+		Updated: 2018-04-21
+#>
+
+	[CmdletBinding()]
+	param
+	(
+		[Parameter(Mandatory = $true,
+				   HelpMessage = 'The ticket subject')]
+		[ValidateNotNullOrEmpty()]
+		[string]$Subject,
+		[Parameter(Mandatory = $true,
+				   HelpMessage = 'The ticket message / body')]
+		[ValidateNotNullOrEmpty()]
+		[string]$Message,
+		[Parameter(Mandatory = $true,
+				   HelpMessage = 'The users full name')]
+		[ValidateNotNullOrEmpty()]
+		[string]$Name,
+		[Parameter(Mandatory = $true,
+				   HelpMessage = 'Email address of the user')]
+		[ValidatePattern('\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*')]
+		[string]$Email,
+		[Parameter(HelpMessage = 'Phone number of the submitter. If the extension is included, use a capital X followed by the extension')]
+		[string]$PhoneNumber = "",
+		[Parameter(HelpMessage = 'The source IP address (can be spoofed)')]
+		[ValidatePattern('\A(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}')]
+		[string]$IpAddress = "",
+		[Parameter(HelpMessage = 'The help topic ID to be associated with the ticket')]
+		[string]$TopicId,
+		[ValidateSet('text/plain', 'text/html', IgnoreCase = $true)]
+		[string]$MessageType = "text/plain",
+		[Parameter(HelpMessage = 'Send an alert to agents on ticket creation')]
+		[boolean]$Alert = $true,
+		[Parameter(Mandatory = $true,
+				   HelpMessage = 'The API key to send to the OSTicket install')]
+		[ValidateNotNullOrEmpty()]
+		[string]$ApiKey,
+		[Parameter(HelpMessage = 'Automatically respond with the ticket info to the email defined in ticket creation')]
+		[boolean]$AutoRespond = $true,
+		[Parameter(Mandatory = $true,
+				   HelpMessage = 'The server to send the API call to. Can be FQDN or IP')]
+		[string]$Server,
+		[Parameter(HelpMessage = 'The priority ID for the new ticket to assume')]
+		[string]$Priority,
+		[Parameter(HelpMessage = 'A list of custom (not present in base install) fields and their desired values')]
+		[hashtable]$CustomFields = @{ },
+		[Parameter(HelpMessage = 'The ticket source type, such as email, phone or API')]
+		[string]$TicketSource = "API",
+		[Parameter(Mandatory = $false,
+				   HelpMessage = 'An array of file paths to attach to the initial message')]
+		[string[]]$Attachments
+	)
+
+	begin {
+		Add-Type -AssemblyName "System.Web"
+	}
+
+	process {
+		$AttachableFiles = @()
+
+		foreach ($FilePath in $Attachments) {
+			$FileToAttach = Get-Item -Path $FilePath
+			if ($FileToAttach.Length -gt 2MB) {
+				Write-Verbose "Attaching file: $($FileToAttach.Name)"
+				$MimeType = [System.Web.MimeMapping]::GetMimeMapping($FilePath.FullName)
+				$Base64FileData = [System.Convert]::ToBase64String([System.IO.File]::ReadAllBytes($FilePath))
+				$AttachableFiles = $AttachableFiles + @(@{ ($FileToAttach.Name) = "data:$MimeType;base64,$Base64FileData" })
+			} else {
+				Write-Warning "Skipping file: $($FileToAttach.Name), as it is over the 2MB limit"
+			}
+		}
+
+		$Params = $CustomFields + @{
+			# Required
+			name = $Name;
+			email = $Email;
+			subject = $Subject;
+			message = $Message;
+
+			# optional
+			phone = $PhoneNumber;
+			ip = $IpAddress;
+			topicID = $topicID;
+			alert = $Alert;
+			autorespond = $AutoRespond;
+			messagetype = $MessageType;
+			source = $TicketSource;
+			priority = $Priority;
+			attachments = $AttachableFiles
+		}
+
+		# Add the correct JSON endpoint
+		$URI = "$Server/api/tickets.json"
+		$HTTPResponse = Invoke-WebRequest -Uri $URI -Headers @{ "X-API-Key" = $APIKey } -Body $Params -UseDefaultCredentials -Method Post -ContentType "application/json"
+
+		# If it fails for whatever reason, let us know
+		if ($HTTPResponse.StatusCode -ne 201) {
+			Write-Error "Unexpected HTTP status code: $($HTTPResponse.StatusCode)"
+			Write-Error $HTTPResponse.RawContent
+		}
+	}
+
+	end {
+
+	}
 }
